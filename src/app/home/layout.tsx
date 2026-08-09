3d1d700d35e816fc5ccf5358bf6619fa37150d4c
stitched-sand. <sandraservat@gmail.com.>
2026-08-09 19:00:07 -0400

    feat: build right sidebar sections

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,15 +1,18 @@
-import LeftSidebar from '@/components/LeftSidebar'
-import React from 'react'
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
+import React from "react";
 
-export default function Layout({children} : Readonly<{ children: React.ReactNode }>) {
+export default function Layout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
-    <LeftSidebar />
-    <div className="mr-2 md:mr-10 xl:mr-110 lg:ml-100 ml-12 min-h-screen border border-border mb-20"></div>
-      {children}
+      <LeftSidebar />
+      <div className="mr-2 md:mr-10 xl:mr-110 lg:ml-100 ml-12 min-h-screen border border-border mb-20">
+         {children}
+      </div>
+      <RightSidebar />
+     
     </>
-  )
+  );
 }
-
-
-
