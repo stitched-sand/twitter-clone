b1d0704e6af62baaba96f52cd3be6ab35565a06f
stitched-sand. <sandraservat@gmail.com.>
2026-08-10 20:21:44 -0400

    feat: add hardcoded posts to homepage

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CreatePost from "@/components/CreatePost";
+import Posts from "@/components/Posts";
 
 export default function Page() {
   return (
@@ -14,6 +15,7 @@ export default function Page() {
         </button>
       </div>
       <CreatePost/>
+      <Posts/>
     </div>
   );
 }
